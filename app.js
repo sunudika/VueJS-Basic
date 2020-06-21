66fa4ab32decdacb59c555000f7bc0f12b9955c8
Sunu Ilham Pradika <sunudika@gmail.com>
2020-06-21 20:12:44 +0700

    Checkpoint: Two
    Interpolation Template

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        maximum: 50,
-        products: null,
-        cart: []
-    },
-    mounted: function() {
-        fetch("https://hplussport.com/api/products/order/price")
-        .then(response => response.json())
-        .then(data => {
-            this.products = data;
-        });
-    },
-    methods: {
-        addItem: function(product) {
-            this.cart.push(product);
-        }
-    },
+        name: "Fluffy Ski Coat"
+    }
 });
