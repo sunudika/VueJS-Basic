31cdca1d1ff6528d5bf9f487ccef32d5ba724b3a
Sunu Ilham Pradika <sunudika@gmail.com>
2020-06-22 23:02:57 +0700

    Checkpoint: Five
    all done

--- app.js
+++ app.js
@@ -1,32 +1,87 @@
 var app = new Vue({
     el: '#app',
     data: {
-        slugText: 'The Quick #(*!&@ Brown 29'
+        maximum: 50,
+        products: null,
+        cart: [],
+        style: {
+            label: ['font-weight-bold', 'mr-2'],
+            inputWidth: 60,
+            sliderStatus: false
+        }
+    },
+    mounted: function() {
+        fetch("https://hplussport.com/api/products/order/price")
+        .then(response => response.json())
+        .then(data => {
+            this.products = data;
+        });
     },
     computed: {
-        slugetize: function() {
-            return this.slugText
-                .toLowerCase()
-                .replace(/[^\w ]+/g, '')
-                .replace(/ +/g, '-') + '-' + this.now
+        sliderState: function() {
+            return this.style.sliderStatus ? 'd-flex' : 'd-none'
         },
-        now: function() {
-            var date = new Date()
-            return(
-                String(date.getHours()) +
-                String(date.getMinutes()) +
-                String(date.getSeconds()) 
-            )
+        cartTotal: function() {
+            let sum = 0
+            for (key in this.cart) {
+                sum = sum + (this.cart[key].product.price * this.cart[key].qty)
+            }
+            return sum;
+        },
+        cartQty: function() {
+            let qty = 0
+            for (key in this.cart) {
+                qty = qty + this.cart[key].qty
+            }
+            return qty;
+        },
+    },
+    filters: {
+        currencyFormat: function (value) {
+            return '$' + Number.parseFloat(value).toFixed(2)
         }
     },
     methods: {
-        // now: function() {
-        //     var date = new Date()
-        //     return(
-        //         String(date.getHours()) +
-        //         String(date.getMinutes()) +
-        //         String(date.getSeconds()) 
-        //     )
-        // }
+        before: function(element) {
+            element.className = "d-none"
+        },
+        enter: function(element) {
+            var delay = element.dataset.index * 100
+            setTimeout(function() {
+                element.className = "row d-flex mb-3 align-items-center animated fadeInRight"
+            }, delay)
+        },
+        leave: function(element) {
+            var delay = element.dataset.index * 100
+            setTimeout(function() {
+                element.className = "row d-flex mb-3 align-items-center animated fadeOutRight"
+            }, delay)
+        },
+        addItem: function(product) {
+            var productIndex;
+            var productExist = this.cart.filter(function (item, index) {
+                if (item.product.id == Number(product.id)) {
+                    productIndex = index;
+                    return true
+                } else {
+                    return false
+                }
+            })
+
+            if (productExist.length) {
+                this.cart[productIndex].qty++
+            } else {
+                this.cart.push({ product: product, qty: 1 })
+            }
+        },
+        deleteItem: function (key) {
+            if (this.cart[key].qty > 1) {
+                this.cart[key].qty--
+            } else {
+                this.cart.splice(key, 1)
+            }
+        }
+        
+        
     },
 });
